57a6d587274942e1f78e1609087b91600bad1f79
Felipe S. Mezzarana <spinelli.mezzarana@gmail.com>
2024-03-24 18:11:37 +0100

    Ensures temp directory exist

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Check if temp directory exists, create if not.
+directory="temp"
+if [ ! -d "$directory" ]; then
+    echo "The directory $directory does not exist. Creating..."
+    mkdir "$directory"
+    echo "Directory created successfully!"
+else
+    echo "The directory $directory already exists."
+fi
+
 TAG=stock-analizer
 docker build -f Dockerfile -t $TAG .
 docker run \
